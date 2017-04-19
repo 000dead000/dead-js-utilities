0418f2bdbc84f588f973911f265744caa6e2f265
000PaRaDoX000 <ariel.calzada@gmail.com>
2017-04-18 22:43:10 -0500

    MOved mixins

--- dead_js_utilities/mixins/__init__.py
+++
@@ -1,3 +0,0 @@
-from ajax_response import *
-from ensure_csrf import *
-from json_response import *
--- dead_js_utilities/mixins/ensure_csrf.py
+++
@@ -1,8 +0,0 @@
-from django.utils.decorators import method_decorator
-from django.views.decorators.csrf import ensure_csrf_cookie
-
-
-class EnsureCSRFMixin(object):
-    @method_decorator(ensure_csrf_cookie)
-    def dispatch(self, *args, **kwargs):
-        return super(EnsureCSRFMixin, self).dispatch(*args, **kwargs)
